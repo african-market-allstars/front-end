45264ca63c6d28d95605d821c2f7d4a1c51bce3a
cyamos2 <cyamos2@yahoo.com>
2021-04-22 21:44:02 -0400

    added CSS

---
+++ front-end/src/Components/About.js
@@ -0,0 +1,15 @@
+export default function About(){
+
+
+    return(
+        <div>
+            <h1>Who we are</h1>
+        <p>Since 3500, it has been our mission to connect customers with our clients. We help our clients strive to reach out to consumers around the world!</p>
+
+        <h2>Contact</h2>
+        <p>Email: CustomerService@safrica.com</p>
+        <p>Phone: 1-800-233-2012 (Available 10am - 7pm EST all 7 days of the week)</p>
+        <p>Address: 2313 W. Chestnut Dr., Norfolk, Virginia 23324 United States</p>
+        </div>
+    )
+}
